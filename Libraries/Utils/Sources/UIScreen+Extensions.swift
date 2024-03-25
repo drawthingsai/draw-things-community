0e6dd792694f663cfbca5b91b3c3f5ad74553cea
Liu Liu <i@liuliu.me>
2024-03-25 20:46:02 +0000

    Gate UIScreen to UIKit import.

--- Libraries/Utils/Sources/UIScreen+Extensions.swift
+++ Libraries/Utils/Sources/UIScreen+Extensions.swift
@@ -1,8 +1,10 @@
-import UIKit
+#if canImport(UIKit)
+  import UIKit
 
-extension UIScreen {
-  public var isAllScreen: Bool {
-    // If the ratio is larger than 16:9, it is all screen (somewhere around 21:10).
-    return bounds.height / bounds.width > 1.8
+  extension UIScreen {
+    public var isAllScreen: Bool {
+      // If the ratio is larger than 16:9, it is all screen (somewhere around 21:10).
+      return bounds.height / bounds.width > 1.8
+    }
   }
-}
+#endif
