24477c84a07e37e9d2d848ce0ec481dbf98d5f6c
Liu Liu <i@liuliu.me>
2024-03-25 19:26:15 -0400

    Add new script. Change to two different WORKSPACE.

---
+++ Apps/ModelConverter/Converter.swift
@@ -0,0 +1,32 @@
+import ArgumentParser
+import Foundation
+import ModelOp
+import ModelZoo
+import NNC
+
+@main
+struct Converter: ParsableCommand {
+  @Option(
+    name: .shortAndLong,
+    help: "The model file that is either the safetensors or the PyTorch checkpoint.")
+  var file: String
+  @Option(name: .shortAndLong, help: "The name of the model.")
+  var name: String
+  @Option(help: "The autoencoder file.")
+  var autoencoderFile: String? = nil
+  @Option(name: .shortAndLong, help: "The directory to write the output files to.")
+  var outputDirectory: String
+  @Flag(help: "Whether to convert the text encoder(s).")
+  var textEncoders = false
+
+  mutating func run() throws {
+    ModelZoo.externalUrl = URL(fileURLWithPath: outputDirectory)
+    let importer = ModelImporter(
+      filePath: file, modelName: name, isTextEncoderCustomized: textEncoders,
+      autoencoderFilePath: autoencoderFile)
+    let result = try importer.import { _ in
+    } progress: { _ in
+    }
+    print(result)
+  }
+}
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -20,11 +20,13 @@ OS=$(uname -s)
 
 if [ "$OS" == "Darwin" ]; then
   echo "try-import %workspace%/.bazelrc.darwin" > $GIT_ROOT/.bazelrc
+  ln -s $GIT_ROOT/WORKSPACE.darwin $GIT_ROOT/WORKSPACE
   # Install realpath
   brew install coreutils
   # Install xcode cmd line tools
   xcode-select --install
 else
   echo "try-import %workspace%/.bazelrc.linux" > $GIT_ROOT/.bazelrc
+  ln -s $GIT_ROOT/WORKSPACE.linux $GIT_ROOT/WORKSPACE
   $GIT_ROOT/Scripts/setup_clang.sh /usr/local
 fi
