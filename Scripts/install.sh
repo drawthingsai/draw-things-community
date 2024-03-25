4918713bb77baf37e7084e76a0fcc61857f9593b
Liu Liu <i@liuliu.me>
2024-03-25 16:00:27 -0400

    Update to include relevant scripts to install.

---
+++ Scripts/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GIT_CONFIG=$(git rev-parse --git-dir)
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+mkdir -p $GIT_CONFIG/hooks/pre-commit.d
+
+rm -f $GIT_CONFIG/hooks/pre-commit
+ln -s $GIT_ROOT/Scripts/vendors/dispatch $GIT_CONFIG/hooks/pre-commit
+
+rm -f $GIT_CONFIG/hooks/pre-commit.d/swift-format
+ln -s $GIT_ROOT/Scripts/swift-format/pre-commit $GIT_CONFIG/hooks/pre-commit.d/swift-format
+
+rm -f $GIT_CONFIG/hooks/pre-commit.d/buildifier
+ln -s $GIT_ROOT/Scripts/buildifier/pre-commit $GIT_CONFIG/hooks/pre-commit.d/buildifier
+
+OS=$(uname -s)
+
+if [ "$OS" == "Darwin" ]; then
+  echo "try-import %workspace%/.bazelrc.darwin" > $GIT_ROOT/.bazelrc
+  # Install realpath
+  brew install coreutils
+  # Install xcode cmd line tools
+  xcode-select --install
+else
+  echo "try-import %workspace%/.bazelrc.linux" > $GIT_ROOT/.bazelrc
+  $GIT_ROOT/Scripts/setup_clang.sh /usr/local
+fi
