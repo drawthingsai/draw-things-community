7064b639397ef0e2b91e2629e137d52b9a6c20f1
Liu Liu <i@liuliu.me>
2024-03-26 14:05:02 -0400

    Add JSON output for ModelConverter.

--- Apps/ModelConverter/Converter.swift
+++ Apps/ModelConverter/Converter.swift
@@ -1,4 +1,5 @@
 import ArgumentParser
+import Diffusion
 import Foundation
 import ModelOp
 import ModelZoo
@@ -19,15 +20,64 @@ struct Converter: ParsableCommand {
   @Flag(help: "Whether to convert the text encoder(s).")
   var textEncoders = false
 
+  private struct Specification: Codable {
+    var name: String
+    var file: String
+    var version: ModelVersion
+    var modifier: SamplerModifier
+    var textEncoder: String?
+    var autoencoder: String?
+    var clipEncoder: String?
+  }
+
   mutating func run() throws {
     ModelZoo.externalUrl = URL(fileURLWithPath: outputDirectory)
+    let fileName = Importer.cleanup(filename: name)
     let importer = ModelImporter(
-      filePath: file, modelName: Importer.cleanup(filename: name),
+      filePath: file, modelName: fileName,
       isTextEncoderCustomized: textEncoders,
       autoencoderFilePath: autoencoderFile)
     let result = try importer.import { _ in
     } progress: { _ in
     }
-    print(result)
+    let fileNames = result.0.map { ($0 as NSString).lastPathComponent }
+    let modelVersion = result.1
+    let modifier = result.2
+    var clipEncoder: String? = nil
+    let textEncoder: String?
+    switch modelVersion {
+    case .v1:
+      textEncoder = fileNames.first {
+        $0.hasSuffix("_clip_vit_l14_f16.ckpt")
+      }
+    case .v2:
+      textEncoder = fileNames.first {
+        $0.hasSuffix("_open_clip_vit_h14_f16.ckpt")
+      }
+    case .sdxlBase, .ssd1b:
+      clipEncoder = fileNames.first {
+        $0.hasSuffix("_clip_vit_l14_f16.ckpt")
+      }
+      textEncoder = fileNames.first {
+        $0.hasSuffix("_open_clip_vit_bigg14_f16.ckpt")
+      }
+    case .sdxlRefiner:
+      textEncoder = fileNames.first {
+        $0.hasSuffix("_open_clip_vit_bigg14_f16.ckpt")
+      }
+    case .kandinsky21, .svdI2v, .wurstchenStageC, .wurstchenStageB:
+      fatalError()
+    }
+    let autoencoder = fileNames.first {
+      $0.hasSuffix("_vae_f16.ckpt")
+    }
+    let specification = Specification(
+      name: name, file: "\(fileName)_f16.ckpt", version: modelVersion, modifier: modifier,
+      textEncoder: textEncoder, autoencoder: autoencoder, clipEncoder: clipEncoder)
+    let jsonEncoder = JSONEncoder()
+    jsonEncoder.keyEncodingStrategy = .convertToSnakeCase
+    jsonEncoder.outputFormatting = .prettyPrinted
+    let jsonData = try jsonEncoder.encode(specification)
+    print(String(decoding: jsonData, as: UTF8.self))
   }
 }
