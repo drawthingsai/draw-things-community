4ef5933f66b4784e992a131cfb596263f005ba0a
Liu Liu <i@liuliu.me>
2024-03-26 16:55:45 +0000

    Move cleanup function into ModelOp.

--- Libraries/ModelOp/Sources/Importer.swift
+++ Libraries/ModelOp/Sources/Importer.swift
@@ -4,3 +4,18 @@ public enum Importer {
   public static let queue = DispatchQueue(
     label: "com.draw-things.model-import", qos: .userInitiated)
 }
+
+extension Importer {
+  public static func cleanup(filename: String) -> String {
+    // Convert the model name into file name.
+    var finalName = ""
+    for character in filename {
+      if character.isASCII && (character.isLetter || character.isNumber) {
+        finalName += String(character)
+      } else {
+        finalName += "_"
+      }
+    }
+    return finalName.lowercased()
+  }
+}
