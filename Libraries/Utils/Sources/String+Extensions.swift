2f1a8caf20254d6a629938fd5ef3185dfd700cc4
Liu Liu <i@liuliu.me>
2024-07-22 22:35:46 +0000

    Fix case the chinese is not show up in the saved file name.

--- Libraries/Utils/Sources/String+Extensions.swift
+++ Libraries/Utils/Sources/String+Extensions.swift
@@ -14,3 +14,41 @@ extension String {
     }
   }
 }
+
+extension Character {
+  public var isHan: Bool {
+    for scalar in unicodeScalars {
+      switch scalar.value {
+      case 19968...40959:  // Common
+        return true
+      case 13312...19903:  // Rare
+        return true
+      case 131072...173791:  // Rare, historic
+        return true
+      case 173824...177983:  // Rare, historic
+        return true
+      case 177984...178207:  // Uncommon
+        return true
+      case 63744...64255:  // Duplicates
+        return true
+      case 194560...195103:  // Unifiable variants
+        return true
+      case 40870...40883:  // Interoperability with HKSCS standard
+        return true
+      case 40884...40891:  // Interoperability with GB 18030 standard
+        return true
+      case 40892...40898:  // Interoperability with commercial implementations
+        return true
+      case 40899:  // Correction of mistaken unification
+        return true
+      case 40900...40902:  // Interoperability with ARIB standard
+        return true
+      case 40903...40907:  // Interoperability with HKSCS standard
+        return true
+      default:
+        continue
+      }
+    }
+    return false
+  }
+}
