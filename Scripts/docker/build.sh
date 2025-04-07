1b89fa041209585b33197bd37dbc52932b31171c
Liu Liu <i@liuliu.me>
2025-04-07 21:22:55 +0000

    Use 6.0.3 Swift for base image.

--- Scripts/docker/build.sh
+++ Scripts/docker/build.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker build -f cudaSwiftDockerfile -t cuda-swift:cuda12.4.1-cudnn-runtime-ubuntu22.04-swift5.10.1 .
+docker build -f cudaSwiftDockerfile -t cuda-swift:cuda12.4.1-cudnn-runtime-ubuntu22.04-swift6.0.3 .
