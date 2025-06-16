8c66a8d4beb9c357a5c74e9d8ed70235ff4ba5f9
Liu Liu <i@liuliu.me>
2025-06-16 23:43:30 +0000

    Minor update on the protobuf generated code.

--- Libraries/GRPC/Models/Sources/imageService/imageService.pb.swift
+++ Libraries/GRPC/Models/Sources/imageService/imageService.pb.swift
@@ -1067,11 +1067,15 @@ extension ImageGenerationRequest: SwiftProtobuf.Message, SwiftProtobuf._MessageI
     var _sharedSecret: String? = nil
     var _chunked: Bool = false
 
+    #if swift(>=5.10)
       // This property is used as the initial default value for new instances of the type.
       // The type itself is protecting the reference to its storage via CoW semantics.
       // This will force a copy to be made of this reference when the first mutation occurs;
       // hence, it is safe to mark this as `nonisolated(unsafe)`.
       static nonisolated(unsafe) let defaultInstance = _StorageClass()
+    #else
+      static let defaultInstance = _StorageClass()
+    #endif
 
     private init() {}
 
