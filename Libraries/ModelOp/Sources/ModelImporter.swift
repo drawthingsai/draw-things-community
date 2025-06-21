909b8149630401901b4b013a6499c7cfecf3eb1e
Liu Liu <i@liuliu.me>
2025-06-21 00:06:42 +0000

    Make sure we remove prefixes for Hunyuan / Wan models import.

--- Libraries/ModelOp/Sources/ModelImporter.swift
+++ Libraries/ModelOp/Sources/ModelImporter.swift
@@ -1074,6 +1074,24 @@ public final class ModelImporter {
             stateDict[String(key.dropFirst(6))] = value
           }
         }
+      } else if modelVersion == .hunyuanVideo {
+        // Remove the model.diffusion_model / model prefix.
+        for (key, value) in stateDict {
+          if key.hasPrefix("model.diffusion_model.") {
+            stateDict[String(key.dropFirst(22))] = value
+          } else if key.hasPrefix("model.") {
+            stateDict[String(key.dropFirst(6))] = value
+          }
+        }
+      } else if modelVersion == .wan21_1_3b || modelVersion == .wan21_14b {
+        // Remove the model.diffusion_model / model prefix.
+        for (key, value) in stateDict {
+          if key.hasPrefix("model.diffusion_model.") {
+            stateDict[String(key.dropFirst(22))] = value
+          } else if key.hasPrefix("model.") {
+            stateDict[String(key.dropFirst(6))] = value
+          }
+        }
       }
       // In case it is not on high performance device and it is SDXL model, read the parameters directly from the mapping.
       if let unetReader = unetReader {
