ddf79d0c83b33f3bc75fe5705e16f2e91663be9a
Liu Liu <i@liuliu.me>
2026-02-10 04:48:36 +0000

    Update to CUDA 12.8.1 as base.

--- Scripts/docker/build.sh
+++ Scripts/docker/build.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker build -f cudaSwiftDockerfile -t cuda-swift:cuda12.4.1-cudnn-runtime-ubuntu22.04-swift6.0.3 .
+docker build -f cudaSwiftDockerfile -t cuda-swift:cuda12.8.1-cudnn-runtime-ubuntu22.04-swift6.0.3 .
