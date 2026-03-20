ec0eb187e677d70794ea843705949e666310d086
Liu Liu <i@liuliu.me>
2026-03-20 22:58:09 +0000

    Still buggy.

--- Package.swift
+++ Package.swift
@@ -21,11 +21,11 @@ let package = Package(
       ] : []),
   dependencies: [
     .package(
-      url: "https://github.com/liuliu/ccv.git", revision: "2bb13063e84dfc6ccbc23411fc09c069c1e6d2d5"
+      url: "https://github.com/liuliu/ccv.git", revision: "2238a1c0f8ce2584d6f9315ccd21f98397c52914"
     ),
     .package(
       url: "https://github.com/liuliu/s4nnc.git",
-      revision: "59f852c31087f029b4ef88b1d30a3436606e95ab"),
+      revision: "8ce1b14cc9a8d75cf3b5419536cd5e1ee7972090"),
     .package(
       url: "https://github.com/liuliu/dflat.git",
       revision: "73925e51e4f44add842177a229f9990cb13711ff"),
